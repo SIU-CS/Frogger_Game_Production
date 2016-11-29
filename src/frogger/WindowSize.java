81919a15d322706e3d26fbbe7dc5394a4b0cade7
Devon Guinn <devonguinn@siu.edu>
2016-11-29 12:58:26 -0600

    fix

--- src/frogger/WindowSize.java
+++ src/frogger/WindowSize.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WindowSize {
-
+	public void setUp(){
+		
+		Background game = new Background();
+	}
 	@Test
 	public void test() {
 		assertEquals(GameTools.boardHeight,600);
@@ -13,5 +16,6 @@ public class WindowSize {
 		assertEquals(GameTools.columnWidth,0);
 		assertEquals(GameTools.rowHeight,0);
 	}
-
+	
+	
 }
